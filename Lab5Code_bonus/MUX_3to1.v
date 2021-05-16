7dfbb6193ab4e3c7bbb123bd88a911cf2735f849
yichi170 <ilee900620@gmail.com>
2021-05-16 17:39:15 +0800

    fix file in bonus

---
+++ Lab5Code_bonus/MUX_3to1.v
@@ -0,0 +1,21 @@
+/***************************************************
+Student Name: 李懿麒 陳品戎
+Student ID: 0816032 0816102
+***************************************************/
+
+`timescale 1ns/1ps
+
+module MUX_3to1(
+	input   	[32-1:0] data0_i,       
+	input   	[32-1:0] data1_i,
+	input   	[32-1:0] data2_i,
+	input       [ 2-1:0] select_i,
+	output wire [32-1:0] data_o
+);
+
+	assign data_o = (select_i == 0) ? data0_i : (
+					((select_i == 1) ? data1_i : 
+					data2_i));
+
+endmodule      
+          
